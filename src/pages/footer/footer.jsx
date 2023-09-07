38ae956d4c497072cd35324722671d9d9a622a37
Innis <1575270674@qq.com>
2023-09-07 16:50:52 +0800

    feat: add footer links

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -4,22 +4,37 @@ import {YoutubeIcon} from "@/graphics/YoutubeIcon";
 import {ThreadsIcon} from "@/graphics/ThreadsIcon";
 import React from "react";
 import LanguageSelect from "@/pages/footer/LanguageSelect";
+import Link from "next/link";
 
 
 export default function Footer(){
 
     return (
-        <div className={"p-[50px]  text-sm"}>
-            <div className={"flex flex-row justify-between items-center w-[180px]"}>
-                <FacebookIcon />
-                <YoutubeIcon />
-                <InstagramIcon />
-                <ThreadsIcon />
+        <div className={"p-[50px]  text-sm flex flex-row justify-between"}>
+            <div>
+                <div className={"flex flex-row justify-between items-center w-[180px]"}>
+                    <FacebookIcon />
+                    <YoutubeIcon />
+                    <InstagramIcon />
+                    <ThreadsIcon />
+                </div>
+                <div className={"mt-6"}>
+                    <LanguageSelect />
+                </div>
+                <div className={"font-bold mt-6 text-[#808080]"}>© FGACYC</div>
             </div>
-            <div className={"mt-6"}>
-                <LanguageSelect />
+            <div className={"flex flex-col justify-between text-[#808080] h-[110px]"}>
+                <Link href={"/"} className={"hover:underline"}>Home</Link>
+                <Link href={"/"} className={"hover:underline"}>Terms and Conditions</Link>
+                <Link href={"/"} className={"hover:underline"}>Privacy Policy</Link>
+                <Link href={"/"} className={"hover:underline"}>Collection Statemen</Link>
             </div>
-            <div className={"font-bold mt-6 text-[#808080]"}>© FGACYC</div>
+            <div className={"flex flex-col justify-between text-[#808080] h-[110px]"}>
+                <Link href={"/"} className={"hover:underline"}>Help</Link>
+                <Link href={"/"} className={"hover:underline"}>Manage Account</Link>
+                <Link href={"/"} className={"hover:underline"}>Media Center</Link>
+                <Link href={"/"} className={"hover:underline"}>Investor Relations</Link>
+            </div>
         </div>
     )
 }
