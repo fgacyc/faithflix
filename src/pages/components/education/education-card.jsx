7f272bb2a7cfa1b0a40250476fd20527e160bb3b
Innis <1575270674@qq.com>
2023-09-28 11:25:09 +0800

    feat: course page connect to cms

--- src/pages/components/education/education-card.jsx
+++ src/pages/components/education/education-card.jsx
@@ -3,58 +3,39 @@ import {BsFillPlayFill} from "react-icons/bs";
 import {Button} from "@nextui-org/react";
 import {useRouter} from "next/router";
 
-export default function EducationCard({index}){
-    const videoData= {
-        id: 1,
-        title: "There is a miracle in your mouth",
-        cover: "/images/There_is_ a_miracle.jpg",
-        description: "There is a miracle in your mouth There is a miracle in your mouth There is a miracle in your mouth There is a miracle in your mouth",
-        duration: 3600, //seconds
-        year: "2021",
-        creators: ["John Doe", "Jane Doe"],
-        cast: ['John Doe', 'Jane Doe','John Doe', 'Jane Doe'],
-        genres: ["Drama", "Faith"],
-        tags: ['Drama', 'Faith','Drama', 'Faith','Drama', 'Faith'],
-        likes_radio: 0.97,
-        episodes: [
-            "video1","video2","video3","video4","video5","video6","video7","video8","video9","video10",
-        ],
-        clarity: ["1080p", "720p", "480p"],
-        subtitle: ["English", "Chinese"]
-    }
-
+export default function EducationCard({course}){
     const router = useRouter();
     function goTo(routerPath){
         router.push(routerPath)
     }
 
+
     return (
-        <div key={index} className={`flex justify-between items-center p-8 pl-0 
+        <div  className={`flex justify-between items-center p-8 pl-0 
                          border-b border-[#303030] rounded `}
         >
             <Image
                 width={200}
                 height={200}
                 priority={false}
-                src={videoData.cover} alt="video cover"
+                src={`https://cms.fgacyc.com${course.attributes.cover.data.attributes.url}`} alt="video cover"
                 className={"object-cover mr-4 cursor-pointer"}
             />
 
             <div className={"flex flex-col items-start h-[78px] w-9/12 text-white"}>
                 <div className={"flex flex-row justify-between w-full"}>
-                    <div className={""}>{`Unit ${index+1} | ${videoData.duration / 60}m`}</div>
+                    <div className={""}>{`Unit ${course.attributes.index}`}</div>
                     <div>
                         <Button className={"bg-white font-bold rounded mr-[10px] cursor-pointer"}
                                 startContent={<BsFillPlayFill className={"text-4xl"}/>}
-                                isDisabled={index !== 0}
-                                onClick={() => goTo(`/education/msj${index+1}?class=1&type=video`)}
+                                isDisabled={course.id !== 0}
+                                onClick={() => goTo(`/education/msj${course.attributes.index+1}?class=1&type=video`)}
                         >
                             <span className={"relative top-[2px] font-bold"}>Start</span>
                         </Button>
                     </div>
                 </div>
-                <div>this is desc this is desc this is desc this is desc this is desc this is desc
-                </div>
+                <div>{course.attributes.description}</div>
             </div>
         </div>
     );
--- src/pages/education.js
+++ src/pages/education.js
@@ -1,10 +1,13 @@
 import Billboard from "@/pages/components/billboard/billboard";
 import Footer from "@/pages/components/footer/footer";
 import EducationCard from "@/pages/components/education/education-card";
+import {useEffect} from "react";
 
-export default function Education() {
+export default function Education({courses}) {
     // console.log(navigator.language)
     const nums= [1,2,3]
+
+    console.log(courses)
 
     return (
         <div className={"font-martel bg-[#141414]"}>
@@ -13,16 +16,21 @@ export default function Education() {
                 <div className={"mt-10"}>
                     <h1 className="text-white  text-2xl font-bold">My spirit journey</h1>
                     {
-                        nums.map((num,index)=>{
-                            return <EducationCard index={index} key={index}/>
+                        courses.map((course,index)=>{
+                            if(course.attributes.course_type.data.id===1){
+                                return <EducationCard  key={index} course={course}/>
+                            }
+
                         })
                     }
                 </div>
                 <div className={"mt-10"}>
                     <h1 className="text-white   text-2xl font-bold">Connect Group Training</h1>
                     {
-                        nums.map((num,index)=>{
-                            return <EducationCard index={index} key={index}/>
+                        courses.map((course,index)=>{
+                            if(course.attributes.course_type.data.id===2){
+                                return <EducationCard  key={index} course={course}/>
+                            }
                         })
                     }
                 </div>
@@ -31,4 +39,23 @@ export default function Education() {
             <Footer/>
         </div>
     );
+}
+
+export async function getStaticProps() {
+    const options = {
+        method:"GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : `Bearer ${process.env.NEXT_PUBLIC_CMS_API_KEY}`
+        }
+    }
+    const data = await fetch(`https://cms.fgacyc.com/api/courses?populate=*`,options)
+    const res = await data.json()
+    const courses = res.data;
+
+    return {
+        props: {
+            courses
+        }
+    }
 }
