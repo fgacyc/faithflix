f6c06dd5d4da5d7ba33262b7b0f9e967ee358291
Innis <1575270674@qq.com>
2023-09-29 15:03:59 +0800

    feat: result page

--- src/pages/components/education/quizzes.jsx
+++ src/pages/components/education/quizzes.jsx
@@ -3,6 +3,8 @@ import MultipleChoice from "@/pages/components/quiz/multiple-choice";
 import React, {useEffect} from "react";
 import SingleChoice from "@/pages/components/quiz/single-choice";
 import TrueOrFalse from "@/pages/components/quiz/true-or-false";
+import {CircularProgress, Card, CardBody, CardFooter, Chip, Button} from "@nextui-org/react";
+
 
 export default function Quizzes({classID}) {
     // console.log("classID",classID)
@@ -11,6 +13,7 @@ export default function Quizzes({classID}) {
     const [classData, setClassData] = React.useState(null);
     const [allQuizzes, setAllQuizzes] = React.useState(null);
     const [currentQuizIndex, setCurrentQuizIndex] = React.useState(0);
+    const [passed, setPassed] = React.useState(false);
 
     useEffect(() => {
         if (classID === null) return;
@@ -63,36 +66,78 @@ export default function Quizzes({classID}) {
             {
                 allQuizzes &&
                 <div className={"bg-transparent w-full"}>
-                    {
-                        allQuizzes.map((quiz, index) => {
-                            if (quiz.type === "fill_in_blanks") {
-                                return <FillInBlanks key={index}
-                                                     index={index}
-                                                     data={quiz}
-                                                     currentQuizIndex={currentQuizIndex}
-                                                     setCurrentQuizIndex={setCurrentQuizIndex}
-                                ></FillInBlanks>
-                            } else if (quiz.type === "multiple_choice") {
-                                return <MultipleChoice key={index}
-                                                       index={index}
-                                                       data={quiz}
-                                                       currentQuizIndex={currentQuizIndex}
-                                                       setCurrentQuizIndex={setCurrentQuizIndex}></MultipleChoice>
-                            } else if (quiz.type === "single_choice") {
-                                return <SingleChoice key={index}
-                                                     index={index}
-                                                     data={quiz}
-                                                     currentQuizIndex={currentQuizIndex}
-                                                     setCurrentQuizIndex={setCurrentQuizIndex}></SingleChoice>
-                            } else if (quiz.type === "true_or_false") {
-                                return <TrueOrFalse key={index}
-                                                    index={index}
-                                                    data={quiz}
-                                                    currentQuizIndex={currentQuizIndex}
-                                                    setCurrentQuizIndex={setCurrentQuizIndex}></TrueOrFalse>
+                    {/*{*/}
+                    {/*    allQuizzes.map((quiz, index) => {*/}
+                    {/*        if (quiz.type === "fill_in_blanks") {*/}
+                    {/*            return <FillInBlanks key={index}*/}
+                    {/*                                 index={index}*/}
+                    {/*                                 data={quiz}*/}
+                    {/*                                 currentQuizIndex={currentQuizIndex}*/}
+                    {/*                                 setCurrentQuizIndex={setCurrentQuizIndex}*/}
+                    {/*            ></FillInBlanks>*/}
+                    {/*        } else if (quiz.type === "multiple_choice") {*/}
+                    {/*            return <MultipleChoice key={index}*/}
+                    {/*                                   index={index}*/}
+                    {/*                                   data={quiz}*/}
+                    {/*                                   currentQuizIndex={currentQuizIndex}*/}
+                    {/*                                   setCurrentQuizIndex={setCurrentQuizIndex}></MultipleChoice>*/}
+                    {/*        } else if (quiz.type === "single_choice") {*/}
+                    {/*            return <SingleChoice key={index}*/}
+                    {/*                                 index={index}*/}
+                    {/*                                 data={quiz}*/}
+                    {/*                                 currentQuizIndex={currentQuizIndex}*/}
+                    {/*                                 setCurrentQuizIndex={setCurrentQuizIndex}></SingleChoice>*/}
+                    {/*        } else if (quiz.type === "true_or_false") {*/}
+                    {/*            return <TrueOrFalse key={index}*/}
+                    {/*                                index={index}*/}
+                    {/*                                data={quiz}*/}
+                    {/*                                currentQuizIndex={currentQuizIndex}*/}
+                    {/*                                setCurrentQuizIndex={setCurrentQuizIndex}></TrueOrFalse>*/}
+                    {/*        }*/}
+                    {/*    })*/}
+                    {/*}*/}
+                    <div className={"text-white w-full flex flex-col items-center"}>
+                        <div className={"mb-8"}>QUIZ RESULT</div>
+                        <Card className="w-[240px] h-[240px] border-none"  aria-label="card">
+                            <CardBody className="justify-center items-center pb-0">
+                                <CircularProgress
+                                    classNames={{
+                                        svg: "w-36 h-36 drop-shadow-md",
+                                        indicator: "stroke-white",
+                                        track: "stroke-white/10",
+                                        value: "text-3xl font-semibold text-white",
+                                    }}
+                                    value={70}
+                                    strokeWidth={4}
+                                    showValueLabel={true}
+                                    color="success"
+                                    aria-label="process"
+                                />
+                            </CardBody>
+                            <CardFooter className="justify-center items-center pt-0">
+                                <Chip
+                                    classNames={{
+                                        base: "border-1 border-white/30",
+                                        content: "text-white/90 text-small font-semibold",
+                                    }}
+                                    variant="bordered"
+                                >
+                                    YOU GOT 10 /10
+                                </Chip>
+                            </CardFooter>
+                        </Card>
+                        <div className={"mt-6"}>
+                            {
+                                passed ? <div className={"text-green-400"}>YOU PASSED THE QUIZ</div>
+                                    : <div  className={"text-red-400"}>YOU FAILED THE QUIZ</div>
                             }
-                        })
-                    }
+                        </div>
+                        <div className={"mt-6 w-[240px] flex justify-between"}>
+
+                            <Button className={"bg-[rgba(109,109,109,0.3)] text-white rounded"}>Retake</Button>
+                            <Button className={"bg-[rgba(109,109,109,0.3)] text-white rounded"}>Finish</Button>
+                        </div>
+                    </div>
                 </div>
             }
         </>
