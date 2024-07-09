f402306de9fab25bb269f57f348fc5dd4c378840
Innis <1575270674@qq.com>
2024-07-10 02:38:13 +0800

    fix: file import

--- src/pages/components/detail-modal/DetailModal.jsx
+++ src/pages/components/detail-modal/DetailModal.jsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import {Modal, ModalContent, ModalBody} from "@nextui-org/react";
 import CoverButtonCroup from "@/pages/components/detail-modal/CoverButtonGroup";
 import VideoDetailsArea from "@/pages/components/detail-modal/VideoDetailsArea";
-import EpisodesArea from "@/pages/components/detail-modal/EpisodesArea";
 import SimilarVideoArea from "@/pages/components/detail-modal/SimilarVideoArea";
 import VideoAboutArea from "@/pages/components/detail-modal/VideoAboutArea";
 import PubSub from "pubsub-js";
